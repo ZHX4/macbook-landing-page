460f16ddc70492ffc572aec299072f02d2aa78d7
ZHX4 <120254893+ZHX4@users.noreply.github.com>
2026-02-17 00:30:11 +0100

    implement hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,29 @@
-import {navLinks} from '../../constants/index.js'
+import { navLinks } from '../../constants/index.js'
 
 const Navbar = () => {
-  return (
-    <header>
-        <nav>
-            <img src="/logo.svg" alt="Apple Logo"/>
+    return (
+        <header>
+            <nav>
+                <img src="/logo.svg" alt="Apple Logo" />
 
-            <ul>
-                {navLinks.map(({label}) => (
-                    <li key={label}>
-                        <a href={label}>{label}</a>
-                    </li>
-                ))}
-            </ul>
-            <div className="flex-center gap-3">
-                <button>
-                    <img src="/search.svg" alt="Search" />
-                </button>
-                <button>
-                    <img src="/cart.svg" alt="cart" />
-                </button>
-            </div>
-        </nav>
-    </header>
-  )
+                <ul>
+                    {navLinks.map(({ label }) => (
+                        <li key={label}>
+                            <a href={label}>{label}</a>
+                        </li>
+                    ))}
+                </ul>
+                <div className="flex-center gap-3">
+                    <button>
+                        <img src="/search.svg" alt="Search" />
+                    </button>
+                    <button>
+                        <img src="/cart.svg" alt="cart" />
+                    </button>
+                </div>
+            </nav>
+        </header>
+    )
 }
 
 export default Navbar
