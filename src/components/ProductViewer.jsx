e4e05b9636170e23169d775642525dc4998c7e6a
ZHX4 <120254893+ZHX4@users.noreply.github.com>
2026-02-17 22:00:43 +0100

    implement 3D Product viewer section

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -1,16 +1,21 @@
 import useMacbookStore from '../store/index.js'
 import clsx from 'clsx'
 import {Canvas} from '@react-three/fiber'
-import { OrbitControls, Box } from '@react-three/drei'
+import MacbookModel14 from '../components/models/Macbook-14.jsx'
+import StudioLights from './three/StudioLights.jsx'
+import ModelSwitcher from './three/ModelSwitcher.jsx'
+import { useMediaQuery } from 'react-responsive'
 
 const ProductViewer = () => {
     const { color, setColor, scale, setScale } = useMacbookStore()
+
+    const isMobile = useMediaQuery({query: '(max-width: 1024px)'})
     return (
         <section id='product-viewer'>
             <h2>Take a closer look.</h2>
 
             <div className="controls">
-                <p className='info'>MacBookPro 16" in {color}</p>
+                <p className='info'>MacBook Pro | available in 14" & 16" in Space Gray & Dark colors</p>
 
                 <div className="flex-center gap-5 mt-5">
                     <div className="color-control">
@@ -39,9 +44,8 @@ const ProductViewer = () => {
                 </div>
             </div>
             <Canvas id='canvas' camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100 }}>
-                <Box position={[-1,1,0]} scale={10 * scale} material-color={color} />
-
-                <OrbitControls enableZoom={false} />
+                <StudioLights />
+                <ModelSwitcher scale={isMobile ? scale - 0.03 : scale} isMobile={isMobile} />
             </Canvas>
         </section>
     )
